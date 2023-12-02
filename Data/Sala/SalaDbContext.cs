31b75ab3f2f81597a9d820f7915d79be74076c79
Thiago Santos <thiagosantos072@outlook.com>
2023-12-02 20:40:13 -0300

    update teste

--- Data/Sala/SalaDbContext.cs
+++ Data/Sala/SalaDbContext.cs
@@ -19,32 +19,4 @@ public class SalaDbContext : DbContext{
     }
     #endregion
 
-
-    // #region Métodos privados
-    // private List<Sala> RetornaSalas(){
-    //     return new List<Sala>{
-    //         new Sala{
-    //             Id = 1,
-    //             Nome = "Sala 1",
-    //             Capacidade = 10,
-    //             Descricao = "Sala de aula",
-    //             Ativo = true
-    //         },
-    //         new Sala{
-    //             Id = 2,
-    //             Nome = "Sala 2",
-    //             Capacidade = 20,
-    //             Descricao = "Sala de aula",
-    //             Ativo = true
-    //         },
-    //         new Sala{
-    //             Id = 3,
-    //             Nome = "Lab Comp 1",
-    //             Capacidade = 20,
-    //             Descricao = "Laboratório de computação",
-    //             Ativo = true
-    //         }
-    //     };
-    // }
-    // #endregion
 }
